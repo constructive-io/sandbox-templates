fee63850a868763a9d31f24e4c233300207c225e
ghou9khub <noroldburden@gmail.com>
2026-01-27 23:51:27 -0800

    Fix for config. Add codegen in nextjs example

--- graphql/codegen-orm/codegen.config.ts
+++ graphql/codegen-orm/codegen.config.ts
@@ -3,15 +3,6 @@ import { defineConfig } from '@constructive-io/graphql-codegen';
 export default defineConfig({
   endpoint: 'http://api.localhost:3000/graphql',
   output: './src/generated/',
-<<<<<<< Updated upstream
-  reactQuery: {
-    enabled: false,
-  },
-  orm: {
-    output: './src/generated/',
-  },
-=======
   orm: true,
   verbose: true,
->>>>>>> Stashed changes
 });
